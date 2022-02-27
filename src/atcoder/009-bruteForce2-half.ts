5c68550d67fc8090d66fd6542670b7fe5d02f433
shogo-nakano-desu <61229807+shogo-nakano-desu@users.noreply.github.com>
2022-02-27 12:22:21 +0900

    understand bit search algorith,

--- src/atcoder/009-bruteForce2-half.ts
+++ src/atcoder/009-bruteForce2-half.ts
@@ -12,12 +12,23 @@ const S = +nums_first[1];
  */
 const A = input.slice(1, N + 1).map((n) => n.split(" "))[0];
 
+// example
+// N=3
+// S=11
+// A=[2,5,9]
 const main = (N: number, S: number) => {
+  // 1<<Nは２のN乗を表す
+  //i: 0-7で8パターン
   for (let i = 0; i < 1 << N; i++) {
     let sum = 0;
-    for (let j = 0; j <= N; j++) {
-      if (i & (1 << j)) {
-        sum += +A[j];
+    //j: 1-3で3パターン
+    for (let j = 1; j <= N; j++) {
+      // &はビット論理積
+      // iの二進法表記のうち下からj番目が1の時
+      // i=[000,001,010,011,100,101,110,111]
+      // 1<<(j-1)=[001,010,100]
+      if ((i & (1 << (j - 1))) != 0) {
+        sum += +A[j - 1];
       }
     }
     if (sum === S) {
